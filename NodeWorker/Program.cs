2a3b5bb6f4c75ee0744973918de568be79c73baa
Chris Sainty <chris.sainty@quids.com.au>
2012-03-09 12:17:32 +1100

    Initial commit

---
+++ NodeWorker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace NodeWorker
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var start = new ProcessStartInfo
+                {
+                    Arguments = "app/index.js",
+                    FileName = "node.exe",
+                    UseShellExecute = false
+                };
+
+                // Copy all the appSettings into environment variables. This allows them to be accessed from process.env in your node code.
+                foreach (var setting in ConfigurationManager.AppSettings.AllKeys)
+                {
+                    start.EnvironmentVariables.Add(setting, ConfigurationManager.AppSettings[setting]);
+                }
+
+                using (var process = Process.Start(start))
+                {
+                    process.WaitForExit();
+                    Environment.Exit(process.ExitCode); // Pass through the exit code on a successful run. This puts node in control of worker lifetime.
+                }
+            }
+            catch (Exception e)
+            {
+                // Something has gone wrong.
+                // By exiting with a zero code here the process will not be run again until the app is deployed again
+                // In production you may want it to continue running after errors, so exit with a non-zero code
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ NodeWorker/app/index.js
@@ -0,0 +1,10 @@
+﻿// Starting point for your node.js app
+// Make sure all files are set to copy into the output dir
+// Commit all npm modules in your repo as AppHarbor does not support 'npm install' yet
+
+setTimeout(function () {
+    // Simulate doing ome work by waiting a few seconds
+    // Exit with a non-zero code to have your worked run again
+    // Exit with a zero code to have your worker stopped until your next
+    process.exit(0);
+}, 5000);
